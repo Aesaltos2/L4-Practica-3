485ddf2881735c878b8cbf1f7c572f92cf9c5315
Adrian Saltos <Asaltos>
2024-08-07 08:26:10 -0500

    Update URL Home

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/space-tourism-website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
